9139905d3bc3ec317998ed4a1e01e60728e8154b
Amine <amine59510@live.fr>
2021-12-16 19:39:48 +0100

    Ajout du design

---
+++ Design/javascript/script.js
@@ -0,0 +1,8 @@
+// function goBack() {
+//     history.back();
+// }
+// document.getElementById("retour").addEventListener( 'onclick' , goBack());
+
+document.getElementById("retour").addEventListener('click', () => {  
+    history.back();
+})
